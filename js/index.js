d98240813e04b99d228347971ae4ec1113733cbb
Marcelo Spedine <marcelo.spedine23@gmail.com>
2020-11-09 21:06:39 -0300

    Adicionado protótipo de infinitescroll, criada lista em index.html para mapear o que falta ser feito

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 
-function getPokemonPage(pageSize, offset){
+function getNextPokemonPage(pageSize){
   //still on progress, final format wont be in a for loop
-  for(let i=1+offset;i<=pageSize+offset;i++){
+  for(let i=1;i<=pageSize;i++){
     const pokemon = getPokemon(i);
   }
 };
 
 window.addEventListener("scroll", () =>{
   //logic for infinite scroll goes here
+  const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+  
+  if(clientHeight + scrollTop > scrollHeight - 5){
+    getNextPokemonPage(10)
+  }
 });
 
-getPokemonPage(10,0);
+getNextPokemonPage(30,0);
