21e14b9314231943d845abd3004810ebda1217de
Marcelo Spedine <marcelo.spedine23@gmail.com>
2020-11-11 13:40:18 -0300

    final touches

--- js/pokemons.js
+++ js/pokemons.js
@@ -191,7 +191,7 @@ function insertCompletePokemonInfo(pokemon){
   image.src = pokemon.image;
   id.innerText += pokemon.id;
   name.innerText += pokemon.name;
-  weight.innerText += ("" + pokemon.weight + " kg");
+  weight.innerText += ("" + pokemon.weight*0.1 + " kg");
   height.innerText += ("" + (pokemon.height/10) + " m");
   types.innerHTML = generateTypesContent(pokemon.types);
   description.innerText += pokemon.description;
